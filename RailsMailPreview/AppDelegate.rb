604ac0d1a546bb5b07c66c4342b73068b5ac0130
Fernando Barajas <fernyb@fernyb.net>
2011-08-12 17:36:40 -0700

    Initial Commit

---
+++ RailsMailPreview/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  RailsMailPreview
+#
+#  Created by Fernando Barajas on 8/12/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
---
+++ RailsMailPreview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RailsMailPreview
+//
+//  Created by Fernando Barajas on 8/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+  return macruby_main("rb_main.rb", argc, argv);
+}
