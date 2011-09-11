2137ba9fe2856915f0c8e99b1d77bd45010aeb1a
Fernando Barajas <fernyb@fernyb.net>
2011-09-10 21:48:18 -0700

    Adding icons and adding AboutWindowController

---
+++ RailsMailPreview/AboutWindowController.m
@@ -0,0 +1,14 @@
+//
+//  AboutWindowController.m
+//  RailsMailPreview
+//
+//  Created by Fernando Barajas on 9/10/11.
+//  Copyright 2011 Fernando Barajas. All rights reserved.
+//
+
+#import "AboutWindowController.h"
+
+@implementation AboutWindowController
+
+
+@end
