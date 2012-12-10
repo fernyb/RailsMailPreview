a9dbb63d0ce7f89fca07a4976da1afa2e3f4cb0d
Fernando Barajas <fernyb@fernyb.net>
2012-12-09 22:23:15 -0800

    fix for 10.8

---
+++ RailsMailPreview/HDArtificialBridge.h
@@ -0,0 +1,18 @@
+//
+//  HDArtificialBridge.h
+//  RailsMailPreview
+//
+//  Created by Hunter on 11/4/12.
+//  Copyright (c) 2012 Fernando Barajas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HDArtificialBridge : NSObject
+
+-(int)runCGRectGetMaxY:(CGRect) rect;
+-(int)runCGRectGetMaxX:(CGRect) rect;
+-(int)runCGRectGetMinY:(CGRect) rect;
+-(int)runCGRectGetMinX:(CGRect) rect;
+
+@end
